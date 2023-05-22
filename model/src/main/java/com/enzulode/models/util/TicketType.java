085aabdc765f5d16727982cad225bbaf7f58bc4b
Vladislav Andreev <andreevvlad443@gmail.com>
2023-05-23 00:33:39 +0300

    implemented TicketType enumeration & Ticket model

---
+++ model/src/main/java/com/enzulode/models/util/TicketType.java
@@ -0,0 +1,13 @@
+package com.enzulode.models.util;
+
+/** Allowed ticket types */
+public enum TicketType {
+    /** Vip ticket type */
+    VIP,
+
+    /** Usual ticket type */
+    USUAL,
+
+    /** Budgetary ticket type */
+    BUDGETARY;
+}
