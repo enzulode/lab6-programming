7ca4008fcf5595621eda3bcf7f9a1404f6bd12fe
Vladislav Andreev <andreevvlad443@gmail.com>
2023-05-23 00:44:18 +0300

    implemented parser mode enumeration

---
+++ common/src/main/java/com/enzulode/common/parser/ParserMode.java
@@ -0,0 +1,20 @@
+package com.enzulode.common.parser;
+
+/**
+ * Mode selector for parsers
+ *
+ */
+public enum ParserMode
+{
+	/**
+	 * New element is being created
+	 *
+	 */
+	CREATE,
+
+	/**
+	 * Updating existing element
+	 *
+	 */
+	UPDATE;
+}
