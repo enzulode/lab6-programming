a6187626d38ec98a4ad08f8c89f8b8de2fc10a84
Vladislav Andreev <andreevvlad443@gmail.com>
2023-05-26 02:01:50 +0300

    made TypeParser#parseBoolean parsing y/Y as true and other stuff as false + reindented with tabs

--- common/src/main/java/com/enzulode/common/parser/TypesParser.java
+++ common/src/main/java/com/enzulode/common/parser/TypesParser.java
@@ -8,161 +8,157 @@ import com.enzulode.models.util.VenueType;
 public class TypesParser
 {
 
-    /**
-     * Parse string from string
-     *
-     * @param line parsing string
-     * @return parsed string
-     */
-    public static String parseString(String line)
-    {
-        if ("".equals(line.trim())) return null;
+	/**
+	 * Parse string from string
+	 *
+	 * @param line parsing string
+	 * @return parsed string
+	 */
+	public static String parseString(String line)
+	{
+		if ("".equals(line.trim())) return null;
 
-        return line.trim();
-    }
+		return line.trim();
+	}
 
-    /**
-     * Parse integer from string
-     *
-     * @param line parsing string
-     * @return integer value
-     * @throws ParsingException if parsing not succeed
-     */
-    public static Integer parseInteger(String line) throws ParsingException
-    {
-        if ("".equals(line.trim())) return null;
+	/**
+	 * Parse integer from string
+	 *
+	 * @param line parsing string
+	 * @return integer value
+	 * @throws ParsingException if parsing not succeed
+	 */
+	public static Integer parseInteger(String line) throws ParsingException
+	{
+		if ("".equals(line.trim())) return null;
 
-        try
-        {
-            return Integer.parseInt(line);
-        }
+		try
+		{
+			return Integer.parseInt(line);
+		}
 		catch (NumberFormatException e)
 		{
-            throw new ParsingException("Inaproppriate argument type: should be integer", e);
-        }
-    }
+			throw new ParsingException("Inaproppriate argument type: should be integer", e);
+		}
+	}
 
-    /**
-     * Parse long from string
-     *
-     * @param line parsing string
-     * @return long value
-     * @throws ParsingException if parsing not succeed
-     */
-    public static Long parseLong(String line) throws ParsingException
-    {
-        try
-        {
-            return Long.parseLong(line);
-        }
+	/**
+	 * Parse long from string
+	 *
+	 * @param line parsing string
+	 * @return long value
+	 * @throws ParsingException if parsing not succeed
+	 */
+	public static Long parseLong(String line) throws ParsingException
+	{
+		try
+		{
+			return Long.parseLong(line);
+		}
 		catch (NumberFormatException e)
 		{
-            throw new ParsingException("Inaproppriate argument type: should be long");
-        }
-    }
+			throw new ParsingException("Inaproppriate argument type: should be long");
+		}
+	}
 
-    /**
-     * Parse boolean from string
-     *
-     * @param line parsing string
-     * @return boolean value
-     * @throws ParsingException if parsing not succeed
-     */
-    public static Boolean parseBoolean(String line) throws ParsingException
-    {
-        if ("".equals(line.trim())) return null;
+	/**
+	 * Parse boolean from string
+	 *
+	 * @param line parsing string
+	 * @return boolean value
+	 * @throws ParsingException if parsing not succeed
+	 */
+	public static Boolean parseBoolean(String line) throws ParsingException
+	{
+		if ("".equals(line.trim())) return null;
 
-        if ("true".equalsIgnoreCase(line))
+		if ("true".equalsIgnoreCase(line) || "y".equalsIgnoreCase(line))
 		{
-            return true;
-        }
-		else if ("false".equalsIgnoreCase(line))
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Parse TicketType from string
+	 *
+	 * @param line parsing string
+	 * @return TicketType value
+	 * @throws ParsingException if parsing not succeed
+	 */
+	public static TicketType parseTicketType(String line) throws ParsingException
+	{
+		if ("".equals(line.trim())) return null;
+
+		for (TicketType type : TicketType.values())
 		{
-            return false;
-        }
+			if (type.toString().equalsIgnoreCase(line.trim())) return TicketType.valueOf(line);
+		}
 
-        throw new ParsingException("Inaproppriate argument type: should be boolean");
-    }
-
-    /**
-     * Parse TicketType from string
-     *
-     * @param line parsing string
-     * @return TicketType value
-     * @throws ParsingException if parsing not succeed
-     */
-    public static TicketType parseTicketType(String line) throws ParsingException
-    {
-        if ("".equals(line.trim())) return null;
-
-        for (TicketType type : TicketType.values())
-		{
-            if (type.toString().equalsIgnoreCase(line.trim())) return TicketType.valueOf(line);
-        }
-
-        throw new ParsingException(
+		throw new ParsingException(
 				"Inaproppriate argument type: should be TicketType or specified variable not found"
-        );
-    }
+		);
+	}
 
-    /**
-     * Parse VenueType from string
-     *
-     * @param line parsing string
-     * @return VenueType value
-     * @throws ParsingException if parsing not succeed
-     */
-    public static VenueType parseVenueType(String line) throws ParsingException
-    {
-        if ("".equals(line.trim())) return null;
+	/**
+	 * Parse VenueType from string
+	 *
+	 * @param line parsing string
+	 * @return VenueType value
+	 * @throws ParsingException if parsing not succeed
+	 */
+	public static VenueType parseVenueType(String line) throws ParsingException
+	{
+		if ("".equals(line.trim())) return null;
 
-        for (VenueType type : VenueType.values())
+		for (VenueType type : VenueType.values())
 		{
-            if (type.toString().equalsIgnoreCase(line)) return VenueType.valueOf(line);
-        }
+			if (type.toString().equalsIgnoreCase(line)) return VenueType.valueOf(line);
+		}
 
-        throw new ParsingException(
+		throw new ParsingException(
 				"Inaproppriate argument type: should be VenueType or specified variable not found."
-        );
-    }
+		);
+	}
 
-    /**
-     * Parse float from string
-     *
-     * @param line parsing string
-     * @return float value
-     * @throws ParsingException if parsing not succeed
-     */
-    public static Float parseFloat(String line) throws ParsingException
-    {
-        if ("".equals(line.trim())) return null;
+	/**
+	 * Parse float from string
+	 *
+	 * @param line parsing string
+	 * @return float value
+	 * @throws ParsingException if parsing not succeed
+	 */
+	public static Float parseFloat(String line) throws ParsingException
+	{
+		if ("".equals(line.trim())) return null;
 
-        try
-        {
-            return Float.parseFloat(line);
-        }
+		try
+		{
+			return Float.parseFloat(line);
+		}
 		catch (NumberFormatException e)
 		{
-            throw new ParsingException("Inaproppriate argument type: should be float.");
-        }
-    }
+			throw new ParsingException("Inaproppriate argument type: should be float.");
+		}
+	}
 
-    /**
-     * Parse double from string
-     *
-     * @param line parsing string
-     * @return double value
-     * @throws ParsingException if parsing not succeed
-     */
-    public static Double parseDouble(String line) throws ParsingException
-    {
-        try
-        {
-            return Double.parseDouble(line);
-        }
+	/**
+	 * Parse double from string
+	 *
+	 * @param line parsing string
+	 * @return double value
+	 * @throws ParsingException if parsing not succeed
+	 */
+	public static Double parseDouble(String line) throws ParsingException
+	{
+		try
+		{
+			return Double.parseDouble(line);
+		}
 		catch (NumberFormatException e)
 		{
-            throw new ParsingException("Inaproppriate argument type: should be float.");
-        }
-    }
+			throw new ParsingException("Inaproppriate argument type: should be float.");
+		}
+	}
 }
