a2fd2ac5a1ae34cee56c4dfe3fb4686bca463101
Vladislav Andreev <andreevvlad443@gmail.com>
2023-05-25 23:12:46 +0300

    file manipulation service fixed null files as method args & implemented new method for reading all lines of a single file

--- common/src/main/java/com/enzulode/common/filesystem/FileManipulationService.java
+++ common/src/main/java/com/enzulode/common/filesystem/FileManipulationService.java
@@ -1,19 +1,25 @@
 package com.enzulode.common.filesystem;
 
 import com.enzulode.common.filesystem.exception.FileException;
+import lombok.NonNull;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 /** The service for using files, retrieving them by name and other stuff */
-public class FileManipulationService {
+public class FileManipulationService
+{
     /**
      * This method validates file to be writable
      *
      * @param file file for validation
      * @throws FileException if file is not writable
      */
-    public void validateFileIsWritable(File file) throws FileException
+    public void validateFileIsWritable(@NonNull File file) throws FileException
     {
         if (!Files.isWritable(file.toPath()))
             throw new FileException("Specified file is not writable");
@@ -25,7 +31,8 @@ public class FileManipulationService {
      * @param file file for validation
      * @throws FileException if file is not readable
      */
-    public void validateFileIsReadable(File file) throws FileException {
+    public void validateFileIsReadable(@NonNull File file) throws FileException
+    {
         if (!Files.isReadable(file.toPath()))
             throw new FileException("Specified file is not readable");
     }
@@ -36,7 +43,8 @@ public class FileManipulationService {
      * @param file file for validation
      * @throws FileException if file does not exist
      */
-    public void validateFileExists(File file) throws FileException {
+    public void validateFileExists(@NonNull File file) throws FileException
+    {
         if (!file.exists()) throw new FileException("Specified file does not exists");
     }
 
@@ -47,7 +55,8 @@ public class FileManipulationService {
      * @return file object
      * @throws FileException if specified file is not writable, readable or not exists
      */
-    public File getFileByName(String filePath) throws FileException {
+    public File getFileByName(@NonNull String filePath) throws FileException
+    {
         File file = new File(filePath);
 
         validateFileExists(file);
@@ -55,5 +64,21 @@ public class FileManipulationService {
         validateFileIsWritable(file);
 
         return file;
+    }
+
+    public List<String> readFileLines(@NonNull File file) throws FileException
+    {
+//      Check if file exists, and we are able to read from it
+        validateFileExists(file);
+        validateFileIsReadable(file);
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(file)))
+        {
+            return reader.lines().toList();
+        }
+        catch (IOException e)
+        {
+            throw new FileException("Failed to open file", e);
+        }
     }
 }
