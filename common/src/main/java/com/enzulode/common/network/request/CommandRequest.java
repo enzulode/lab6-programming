33bda7457d40b5eeec5568384111536b7d1d2757
Vladislav Andreev <andreevvlad443@gmail.com>
2023-05-27 01:28:44 +0300

    implemented command request

---
+++ common/src/main/java/com/enzulode/common/network/request/CommandRequest.java
@@ -0,0 +1,52 @@
+package com.enzulode.common.network.request;
+
+import com.enzulode.common.command.Command;
+import com.enzulode.common.command.SimpleTicketCommand;
+import com.enzulode.models.Ticket;
+import com.enzulode.network.model.interconnection.Request;
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.net.InetSocketAddress;
+
+/**
+ * This class represents simple command request
+ *
+ */
+@Getter
+public class CommandRequest extends Request
+{
+	/**
+	 * SimpleTicketCommand instance
+	 *
+	 */
+	private final Command<Ticket> command;
+
+	/**
+	 * SimpleCommandRequest constructor without source and destination addresses provided
+	 *
+	 * @param command simple ticket command to be executed
+	 */
+	public CommandRequest(@NonNull Command<Ticket> command)
+	{
+		super();
+		this.command = command;
+	}
+
+	/**
+	 * SimpleCommandRequest constructor
+	 *
+	 * @param from request source address
+	 * @param to request destination address
+	 * @param command command to be executed
+	 */
+	public CommandRequest(
+			@NonNull InetSocketAddress from,
+			@NonNull InetSocketAddress to,
+			@NonNull SimpleTicketCommand command
+	)
+	{
+		super(from, to);
+		this.command = command;
+	}
+}
