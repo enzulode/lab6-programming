72bac6b3ac8d9ccda1332efc143d4efbf87e43b0
Vladislav Andreev <andreevvlad443@gmail.com>
2023-06-01 01:10:10 +0300

    fixed incorrect show output when the database is empty

--- common/src/main/java/com/enzulode/common/command/impl/ShowCommand.java
+++ common/src/main/java/com/enzulode/common/command/impl/ShowCommand.java
@@ -3,6 +3,7 @@ package com.enzulode.common.command.impl;
 import com.enzulode.common.command.SimpleTicketCommand;
 import com.enzulode.common.command.util.ExecutionResult;
 import com.enzulode.common.command.util.ExecutionStatus;
+import com.enzulode.models.Ticket;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
@@ -23,7 +24,12 @@ public class ShowCommand extends SimpleTicketCommand
 			return new ExecutionResult(ExecutionStatus.FAILED, "Command does not consume arguments");
 
 		ExecutionResult result = new ExecutionResult(ExecutionStatus.SUCCEED);
-		dao.findAll().forEach((el) -> result.append(el.toString()));
+
+		List<Ticket> tickets = dao.findAll();
+		if (tickets.size() == 0)
+			return new ExecutionResult(ExecutionStatus.SUCCEED, "No elements found");
+
+		tickets.forEach((el) -> result.append(el.toString()));
 
 		return result;
 	}
