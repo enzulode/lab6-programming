e1093288c3516e56c50857f22111b4f06a8751e1
Vladislav Andreev <andreevvlad443@gmail.com>
2023-06-01 00:55:46 +0300

    implemented serial version UID for command request

--- common/src/main/java/com/enzulode/common/network/request/IdRequest.java
+++ common/src/main/java/com/enzulode/common/network/request/IdRequest.java
@@ -4,9 +4,26 @@ import com.enzulode.network.model.interconnection.Request;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.io.Serial;
+
+/**
+ * This class is an id checking request
+ *
+ */
 @RequiredArgsConstructor
 @Getter
 public class IdRequest extends Request
 {
+	/**
+	 * Serial version UID
+	 *
+	 */
+	@Serial
+	private static final long serialVersionUID = -3346580676163311238L;
+
+	/**
+	 * An id to be checked
+	 *
+	 */
 	private final int id;
 }
