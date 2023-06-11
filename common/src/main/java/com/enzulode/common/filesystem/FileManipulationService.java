cdc8cad4aa087c832a001ec0908753ca95145818
Vladislav Andreev <andreevvlad443@gmail.com>
2023-06-11 17:23:03 +0300

    reindented file with tabs

--- common/src/main/java/com/enzulode/common/filesystem/FileManipulationService.java
+++ common/src/main/java/com/enzulode/common/filesystem/FileManipulationService.java
@@ -14,104 +14,104 @@ import java.util.List;
 /** The service for using files, retrieving them by name and other stuff */
 public class FileManipulationService
 {
-    /**
-     * This method validates file to be writable
-     *
-     * @param file file for validation
-     * @throws FileException if file is not writable
-     */
-    public void validateFileIsWritable(@NonNull File file) throws FileException
-    {
-        if (!Files.isWritable(file.toPath()))
-            throw new FileException("Specified file is not writable");
-    }
+	/**
+	 * This method validates file to be writable
+	 *
+	 * @param file file for validation
+	 * @throws FileException if file is not writable
+	 */
+	public void validateFileIsWritable(@NonNull File file) throws FileException
+	{
+		if (!Files.isWritable(file.toPath()))
+			throw new FileException("Specified file is not writable");
+	}
 
-    /**
-     * This method validates file to be writable
-     *
-     * @param file file for validation
-     * @throws FileException if file is not readable
-     */
-    public void validateFileIsReadable(@NonNull File file) throws FileException
-    {
-        if (!Files.isReadable(file.toPath()))
-            throw new FileException("Specified file is not readable");
-    }
+	/**
+	 * This method validates file to be writable
+	 *
+	 * @param file file for validation
+	 * @throws FileException if file is not readable
+	 */
+	public void validateFileIsReadable(@NonNull File file) throws FileException
+	{
+		if (!Files.isReadable(file.toPath()))
+			throw new FileException("Specified file is not readable");
+	}
 
-    /**
-     * This method validates file to exist
-     *
-     * @param file file for validation
-     * @throws FileNotExistsException if file does not exist
-     */
-    public void validateFileExists(@NonNull File file) throws FileNotExistsException
-    {
-        if (!file.exists()) throw new FileNotExistsException("Specified file does not exists");
-    }
+	/**
+	 * This method validates file to exist
+	 *
+	 * @param file file for validation
+	 * @throws FileNotExistsException if file does not exist
+	 */
+	public void validateFileExists(@NonNull File file) throws FileNotExistsException
+	{
+		if (!file.exists()) throw new FileNotExistsException("Specified file does not exists");
+	}
 
-    /**
-     * Method retrieves file for specified path
-     *
-     * @param filePath requested file path
-     * @return file object
-     * @throws FileException if specified file is not writable, readable or not exists
-     */
-    public File getFileByName(@NonNull String filePath) throws FileException
-    {
-        File file = new File(filePath);
+	/**
+	 * Method retrieves file for specified path
+	 *
+	 * @param filePath requested file path
+	 * @return file object
+	 * @throws FileException if specified file is not writable, readable or not exists
+	 */
+	public File getFileByName(@NonNull String filePath) throws FileException
+	{
+		File file = new File(filePath);
 
-        validateFileExists(file);
-        validateFileIsReadable(file);
-        validateFileIsWritable(file);
+		validateFileExists(file);
+		validateFileIsReadable(file);
+		validateFileIsWritable(file);
 
-        return file;
-    }
+		return file;
+	}
 
-    /**
-     * This method reads all file lines and returns them as a list
-     *
-     * @param file file to fetch its lines
-     * @return a list of file lines
-     * @throws FileException if file doesn't have enough permissions to read or even doesn't exist
-     */
-    public List<String> readFileLines(@NonNull File file) throws FileException
-    {
+	/**
+	 * This method reads all file lines and returns them as a list
+	 *
+	 * @param file file to fetch its lines
+	 * @return a list of file lines
+	 * @throws FileException if file doesn't have enough permissions to read or even doesn't exist
+	 */
+	public List<String> readFileLines(@NonNull File file) throws FileException
+	{
 //      Check if file exists, and we are able to read from it
-        validateFileExists(file);
-        validateFileIsReadable(file);
+		validateFileExists(file);
+		validateFileIsReadable(file);
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(file)))
-        {
-            return reader.lines().toList();
-        }
-        catch (IOException e)
-        {
-            throw new FileException("Failed to open file", e);
-        }
-    }
+		try(BufferedReader reader = new BufferedReader(new FileReader(file)))
+		{
+			return reader.lines().toList();
+		}
+		catch (IOException e)
+		{
+			throw new FileException("Failed to open file", e);
+		}
+	}
 
-    /**
-     * This method creates a BufferedReader for a file by name
-     *
-     * @param filename the name of the file
-     * @return the reader instance
-     * @throws FileException if file doesn't have enough permissions to read or even doesn't exist
-     */
-    public BufferedReader getReaderByName(@NonNull String filename) throws FileException
-    {
-        File file = getFileByName(filename);
+	/**
+	 * This method creates a BufferedReader for a file by name
+	 *
+	 * @param filename the name of the file
+	 * @return the reader instance
+	 * @throws FileException if file doesn't have enough permissions to read or even doesn't exist
+	 */
+	public BufferedReader getReaderByName(@NonNull String filename) throws FileException
+	{
+		File file = getFileByName(filename);
 
 //      Check is file ready to read
-        validateFileExists(file);
-        validateFileIsReadable(file);
+		validateFileExists(file);
+		validateFileIsReadable(file);
 
-        try
-        {
-            return new BufferedReader(new FileReader(file));
-        }
-        catch (IOException e)
-        {
-            throw new FileException("Failed to create file reader", e);
-        }
-    }
+		try
+		{
+			return new BufferedReader(new FileReader(file));
+		}
+		catch (IOException e)
+		{
+			throw new FileException("Failed to create file reader", e);
+		}
+	}
 }
