509d0235d068cc30cb5de7fad9e235a9e64ce257
Vladislav Andreev <andreevvlad443@gmail.com>
2023-06-16 01:13:07 +0300

    using new command factory

--- client/src/main/java/com/enzulode/client/Client.java
+++ client/src/main/java/com/enzulode/client/Client.java
@@ -5,8 +5,11 @@ import com.enzulode.client.parser.cli.TicketCLIParser;
 import com.enzulode.client.parser.cli.VenueCLIParser;
 import com.enzulode.client.util.ConsolePrinter;
 import com.enzulode.client.util.Printer;
+import com.enzulode.client.resolution.ClientCommandFactory;
+import com.enzulode.common.resolution.CommandFactory;
 import com.enzulode.common.resolution.ResolutionService;
 import com.enzulode.common.resolution.ResolutionServiceImpl;
+import com.enzulode.models.Ticket;
 
 import java.util.Scanner;
 
@@ -16,7 +19,8 @@ public class Client
 	{
 		Scanner scanner = new Scanner(System.in);
 		Printer printer = new ConsolePrinter();
-		ResolutionService resolutionService = new ResolutionServiceImpl();
+		CommandFactory<Ticket> commandFactory = new ClientCommandFactory();
+		ResolutionService resolutionService = new ResolutionServiceImpl(commandFactory);
 
 		CoordinatesCLIParser coordinatesParser = new CoordinatesCLIParser(printer, scanner);
 		VenueCLIParser venueParser = new VenueCLIParser(printer, scanner);
