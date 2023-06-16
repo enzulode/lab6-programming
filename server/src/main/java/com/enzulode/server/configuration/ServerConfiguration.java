b3770f7a809272ca08bc5c7245110ce400904736
Vladislav Andreev <andreevvlad443@gmail.com>
2023-06-16 12:24:40 +0300

    server configuration enriched with new command factories & resolution services

--- server/src/main/java/com/enzulode/server/configuration/ServerConfiguration.java
+++ server/src/main/java/com/enzulode/server/configuration/ServerConfiguration.java
@@ -3,6 +3,7 @@ package com.enzulode.server.configuration;
 import com.enzulode.common.dao.TicketDao;
 import com.enzulode.common.execution.ExecutionService;
 import com.enzulode.common.filesystem.FileManipulationService;
+import com.enzulode.common.resolution.GlobalCommandFactory;
 import com.enzulode.common.resolution.CommandFactory;
 import com.enzulode.common.resolution.ResolutionService;
 import com.enzulode.common.resolution.ResolutionServiceImpl;
@@ -10,13 +11,19 @@ import com.enzulode.models.Ticket;
 import com.enzulode.network.UDPSocketServer;
 import com.enzulode.network.exception.NetworkException;
 import com.enzulode.network.handling.RequestHandler;
+import com.enzulode.server.cli.factory.DefaultServerCLIThreadFactoryImpl;
+import com.enzulode.server.cli.factory.ServerCLIThreadFactory;
 import com.enzulode.server.cli.resolution.ServerCommandFactory;
 import com.enzulode.server.execution.ExecutionServiceImpl;
 import com.enzulode.server.handling.DefaultApplicationRequestHandlerImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
  * Application configuration
@@ -38,24 +45,51 @@ public class ServerConfiguration
 	}
 
 	/**
-	 * Command factory bean declaration
+	 * Server command factory bean declaration
 	 *
 	 * @return command factory instance
 	 */
-	@Bean(name = "commandFactory")
-	public CommandFactory<Ticket> commandFactoryBean()
+	@Bean(name = "serverCommandFactory")
+	@Qualifier("serverCommandFactory")
+	public CommandFactory<Ticket> serverCommandFactoryBean()
 	{
 		return new ServerCommandFactory();
 	}
 
 	/**
-	 * Command resolution service bean declaration
+	 * General command factory bean declaration
+	 *
+	 * @return general command factory instance
+	 */
+	@Bean(name = "generalCommandFactory")
+	@Qualifier("generalCommandFactory")
+	public CommandFactory<Ticket> generalCommandFactoryBean()
+	{
+		return new GlobalCommandFactory();
+	}
+
+	/**
+	 * Server command resolution service bean declaration
 	 *
 	 * @param commandFactory command factory instance
 	 * @return command resolution service implementation
 	 */
-	@Bean(name = "commandResolutionService")
-	public ResolutionService commandResolutionServiceBean(CommandFactory<Ticket> commandFactory)
+	@Bean(name = "serverCommandResolutionService")
+	@Qualifier("serverCommandResolutionService")
+	public ResolutionService serverCommandResolutionServiceBean(@Qualifier("serverCommandFactory") CommandFactory<Ticket> commandFactory)
+	{
+		return new ResolutionServiceImpl(commandFactory);
+	}
+
+	/**
+	 * General command resolution service bean declaration
+	 *
+	 * @param commandFactory general command factory instance
+	 * @return command resolution service implementation
+	 */
+	@Bean(name = "generalCommandResolutionService")
+	@Qualifier("generalCommandResolutionService")
+	public ResolutionService generalCommandResolutionServiceBean(@Qualifier("generalCommandFactory") CommandFactory<Ticket> commandFactory)
 	{
 		return new ResolutionServiceImpl(commandFactory);
 	}
@@ -103,10 +137,27 @@ public class ServerConfiguration
 	@Bean(name = "applicationRequestHandler")
 	public RequestHandler applicationRequestHandlerBean(
 			TicketDao dao,
-			ResolutionService resolutionService,
+			@Qualifier("generalCommandResolutionService") ResolutionService resolutionService,
 			ExecutionService<Ticket> executionService
 	)
 	{
 		return new DefaultApplicationRequestHandlerImpl(dao, resolutionService, executionService);
+	}
+
+	/**
+	 * Server CLI thread factory bean declaration
+	 *
+	 * @param resolutionService resolution service instance
+	 * @param executionService execution service instance
+	 * @return server cli thread factory instance
+	 */
+	@Bean(name = "serverCLIThreadFactory")
+	public ServerCLIThreadFactory serverCLIThreadFactoryBean(
+			@Qualifier("serverCommandResolutionService") ResolutionService resolutionService,
+			ExecutionService<Ticket> executionService
+	)
+	{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		return new DefaultServerCLIThreadFactoryImpl(reader, resolutionService, executionService);
 	}
 }
